9d37179013189a4697887ea41abe0f212fa4a5f9
Trishul Raut <mitroz.trishul@gmail.com>
2022-12-04 12:26:01 +0530

    script

---
+++ mcv_demo/mvc_demo_matersoft.sql
@@ -0,0 +1,174 @@
+USE [master]
+GO
+/****** Object:  Database [StudentDB]    Script Date: 04-12-2022 12:21:49 ******/
+CREATE DATABASE [StudentDB]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'StudentDB', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\StudentDB.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'StudentDB_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\StudentDB_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [StudentDB] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [StudentDB].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [StudentDB] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [StudentDB] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [StudentDB] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [StudentDB] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [StudentDB] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [StudentDB] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [StudentDB] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [StudentDB] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [StudentDB] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [StudentDB] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [StudentDB] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [StudentDB] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [StudentDB] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [StudentDB] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [StudentDB] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [StudentDB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [StudentDB] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [StudentDB] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [StudentDB] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [StudentDB] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [StudentDB] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [StudentDB] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [StudentDB] SET RECOVERY FULL 
+GO
+ALTER DATABASE [StudentDB] SET  MULTI_USER 
+GO
+ALTER DATABASE [StudentDB] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [StudentDB] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [StudentDB] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [StudentDB] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [StudentDB] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [StudentDB] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'StudentDB', N'ON'
+GO
+ALTER DATABASE [StudentDB] SET QUERY_STORE = OFF
+GO
+USE [StudentDB]
+GO
+/****** Object:  Table [dbo].[StudentReg]    Script Date: 04-12-2022 12:21:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[StudentReg](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[City] [nvarchar](50) NULL,
+	[Address] [nvarchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  StoredProcedure [dbo].[AddNewStudent]    Script Date: 04-12-2022 12:21:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[AddNewStudent]  
+(  
+   @Name nvarchar (50),  
+   @City nvarchar (50),  
+   @Address nvarchar (100)  
+)  
+as  
+begin  
+
+   Insert into StudentReg values(@Name,@City,@Address)
+ 
+   
+End
+GO
+/****** Object:  StoredProcedure [dbo].[DeleteStudent]    Script Date: 04-12-2022 12:21:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+Create procedure [dbo].[DeleteStudent]  
+(  
+   @StdId int  
+)  
+as   
+begin  
+   Delete from StudentReg where Id=@StdId  
+End
+GO
+/****** Object:  StoredProcedure [dbo].[GetStudentDetails]    Script Date: 04-12-2022 12:21:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE Procedure [dbo].[GetStudentDetails] 
+ 
+as  
+begin  
+
+   select * from StudentReg
+   
+ 
+End
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateStudentDetails]    Script Date: 04-12-2022 12:21:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[UpdateStudentDetails]  
+(  
+   @StdId int,  
+   @Name nvarchar (50),  
+   @City nvarchar (50),  
+   @Address nvarchar (100)  
+)  
+as  
+begin  
+   Update StudentReg   
+   set
+   Name=@Name,  
+   City=@City,  
+   Address=@Address  
+   where Id=@StdId  
+End
+GO
+USE [master]
+GO
+ALTER DATABASE [StudentDB] SET  READ_WRITE 
+GO
